3c61d288ad11f024a180b2d3551d46a090e618be
James30592 <james_everett@live.com>
2022-10-23 21:33:03 +0100

    Mostly done styling portfolio page

---
+++ js/layoutLines.js
@@ -0,0 +1,151 @@
+// import { toggleClass } from "./utils.js";
+
+
+
+initBpAnims();
+drawLayoutLinesOnLoad();
+fadeInElemsOnLoad();
+
+
+
+
+function initBpAnims() {
+  const bps = Array.from(document.querySelectorAll(".lined-bullet-point"));
+  const bpsWithLines = getBpsWithLines(bps);
+  addBpBtnEventListeners(bpsWithLines);
+}
+
+// Get bullet points and their associated elements to be animated as objects.
+function getBpsWithLines(bps) {
+  const bpsWithLines = bps.map(bp => {
+    const bpLine = bp.querySelector(".bp-layout-line");
+    const collapseTargetId = bp.getAttribute("data-bs-target");
+    const collapseDiv = document.querySelector(collapseTargetId);
+  
+    return {
+      bp: bp,
+      line: bpLine,
+      collapseDiv: collapseDiv,
+      midTrans: false
+    };
+  });
+
+  return bpsWithLines
+}
+
+// Add event listeners to buttons with bullet points, to animate bullet point.
+function addBpBtnEventListeners(bpsWithLines) {
+  bpsWithLines.forEach(bpWithLine => {
+    bpWithLine.bp.addEventListener("click", () => {
+      if (!bpWithLine.midTrans) {
+        toggleClass(bpWithLine.line, "activated-bp-layout-line");
+        bpWithLine.midTrans = true;
+  
+        // Only allow one animation per collapse div transition.
+        bpWithLine.collapseDiv.addEventListener("transitionend", () => {
+          bpWithLine.midTrans = false;
+        });
+      };
+    });
+  });
+}
+
+
+
+
+
+function drawLayoutLinesOnLoad() {
+  const allLines = Array.from(document.querySelectorAll(".init-layout-line"));
+  
+  const maxDrawIdx = allLines.reduce((currMaxIdx, currLine) => {
+    return Math.max(currLine.dataset.drawIdx, currMaxIdx);
+  }, 0);
+
+  window.addEventListener("load", () => drawLayoutLines(maxDrawIdx, allLines));
+}
+
+async function drawLayoutLines(maxDrawIdx, allLines) {
+  for (let drawIdx = 0; drawIdx <= maxDrawIdx; drawIdx++) {
+    const theseLines = allLines.filter(line => line.dataset.drawIdx == drawIdx);
+
+    let theseLinesDrawn = new Promise(res => res());
+
+    theseLines.forEach(line => {
+      line.classList.remove("init-layout-line");
+      theseLinesDrawn = awaitTransition(line);
+    });
+
+    await theseLinesDrawn;
+    continue;
+  };
+}
+
+
+
+
+
+
+
+
+
+const FADE_TIME_OFFSET = 100;
+
+function fadeInElemsOnLoad() {
+  const fadeLoadElems = Array.from(document.querySelectorAll(".fade-in-load"));
+  window.addEventListener("load", () => fadeInElems(fadeLoadElems));
+}
+
+function fadeInElems(elems) {
+  if (elems.length === 0) return;
+
+  const currElem = elems.shift();
+
+  currElem.classList.remove("fade-in-load");
+  setTimeout(() => fadeInElems(elems), FADE_TIME_OFFSET);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Returns a promise that resolves when transition on given element ends, 
+// optional transition property name check.
+function awaitTransition(elem, propName = null) {
+  return new Promise(resolve => {
+    elem.addEventListener("transitionend", async evt => {
+
+      if (propName) {
+        if (evt.propertyName === propName) {
+          resolve();
+        }
+
+        else {
+          await awaitTransition(elem, propName);
+          resolve();
+        };
+      }
+
+      else {
+        resolve();
+      };
+
+    }, {once: true});
+  })
+}
+
+function toggleClass(elem, clss) {
+  if (elem.classList.contains(clss)) {
+    elem.classList.remove(clss);
+  }
+  else {
+    elem.classList.add(clss);
+  };
+}
